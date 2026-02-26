0ed3cd13ad0ccb1021fab9dc050b3d8c4f51c35e
Essy-Boni <Esaboni.pro@gmail.com>
2026-02-26 14:45:07 +0100

    TP1 - Tests_Etape 2

--- TP_1/PanierSolution/Panier.Tests/ShoppingCartTests.cs
+++ TP_1/PanierSolution/Panier.Tests/ShoppingCartTests.cs
@@ -5,34 +5,75 @@ namespace Panier.Tests;
 [TestClass]
 public class ShoppingCartTests
 {
-    // Étape 1 — Tests du panier vide
 
-    [TestMethod]
+
+    // Étape 1 — Tests du panier vide
+    #region
+
+
+    [TestMethod] //Un panier neuf contient 0 article.
     public void WhenNewCart_Then0Item()
     {
         var cart = new ShoppingCart();
         Assert.AreEqual(0, cart.GetItemCount());
     }
 
-    [TestMethod]
+    [TestMethod] //Un panier vide a un total égal à 0.
     public void WhenEmptyCart_ThenTotalIs0()
     {
         var cart = new ShoppingCart();
         Assert.AreEqual(0m, cart.GetTotal());
     }
 
-    [TestMethod]
+    [TestMethod] //Appliquer une remise sur un panier vide déclenche une exception.
     public void WhenEmptyCart_ApplyDiscount_ThenException()
     {
         var cart = new ShoppingCart();
         Assert.Throws<InvalidOperationException>(() => cart.ApplyDiscount(10m));
     }
-
+    #endregion
 
     // Étape 2 — Tests d’ajout (validation)
+    #region
+
+    [TestMethod] //Ajouter un article valide augmente le nombre d’articles.
+    public void WhenAddItem_ValidItem_ThenIncreaseItemCount()
+    {
+        var cart = new ShoppingCart();
+        cart.AddItem("Lemon", 2.50m, 2);
+        Assert.AreEqual(1, cart.GetItemCount());
+    }
+
+    [TestMethod] //Ajouter un article avec nom invalide déclenche une exception.
+    public void WhenAddItem_InvalidName_ThenException()
+    {
+        var cart = new ShoppingCart();
+        Assert.Throws<ArgumentException>(() => cart.AddItem("   ", 1m, 1));
+    }
+
+    [TestMethod] //Ajouter un article avec prix ≤ 0 déclenche une exception.
+    public void WhenAddItem_PriceLessOrEqual0_ThenException()
+    {
+        var cart = new ShoppingCart();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => cart.AddItem("Pineapple", 0m, 1));
+        Assert.Throws<ArgumentOutOfRangeException>(() => cart.AddItem("Pineapple", -1m, 1));
+    }
+
+    [TestMethod] //Ajouter un article avec quantité ≤ 0 déclenche une exception.
+    public void WhenAddItem_QtyLessOrEqual0_Throws()
+    {
+        var cart = new ShoppingCart();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => cart.AddItem("Apple", 1m, 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => cart.AddItem("Apple", 1m, -2));
+    }
+    #endregion
+
+
     // Étape 3 — Tests de calcul
     // Étape 4 — Tests de remise
-    
+
 }
 
 
