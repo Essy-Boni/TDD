01e3570582eb8edc3377b3f566b6f31b53614c96
Essy-Boni <Esaboni.pro@gmail.com>
2026-02-26 14:23:52 +0100

    TP1 - Tests_Etape 1

--- TP_1/PanierSolution/Panier.Tests/ShoppingCartTests.cs
+++ TP_1/PanierSolution/Panier.Tests/ShoppingCartTests.cs
@@ -5,6 +5,34 @@ namespace Panier.Tests;
 [TestClass]
 public class ShoppingCartTests
 {
-   
+    // Étape 1 — Tests du panier vide
 
+    [TestMethod]
+    public void WhenNewCart_Then0Item()
+    {
+        var cart = new ShoppingCart();
+        Assert.AreEqual(0, cart.GetItemCount());
+    }
+
+    [TestMethod]
+    public void WhenEmptyCart_ThenTotalIs0()
+    {
+        var cart = new ShoppingCart();
+        Assert.AreEqual(0m, cart.GetTotal());
+    }
+
+    [TestMethod]
+    public void WhenEmptyCart_ApplyDiscount_ThenException()
+    {
+        var cart = new ShoppingCart();
+        Assert.Throws<InvalidOperationException>(() => cart.ApplyDiscount(10m));
+    }
+
+
+    // Étape 2 — Tests d’ajout (validation)
+    // Étape 3 — Tests de calcul
+    // Étape 4 — Tests de remise
+    
 }
+
+
