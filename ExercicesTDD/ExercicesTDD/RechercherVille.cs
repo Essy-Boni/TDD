fb8614bd8c25d3eb6b7eeed9a8205e09f941570b
Essy-Boni <Esaboni.pro@gmail.com>
2026-02-26 11:56:01 +0100

    Projet RechercheVille - fin

--- ExercicesTDD/ExercicesTDD/RechercherVille.cs
+++ ExercicesTDD/ExercicesTDD/RechercherVille.cs
@@ -22,20 +22,16 @@ namespace Exercices
 
         {
             //INITIAL
-            //throw new NotImplementedException();
-
-            //Green 5 : Si le texte de recherche est un « * » (astérisque), il doit renvoyer tous les noms de ville.
-            if (mot == "*")
-                return new List<string>(_villes);
+            throw new NotImplementedException();
 
             // Green 1 : Si le texte de la recherche contient moins de 2 caractères, ***Une exception est levée de type NotFoundException***.
-            if (mot.Length < 2)
-                throw new NotFoundException("Le texte doit contenir au moins 2 caractères.");
+            //if (mot.Length < 2)
+            //    throw new NotFoundException("Le texte doit contenir au moins 2 caractères.");
 
             // Green 2 : Si le texte de recherche est égal ou supérieur à 2 caractères, il doit renvoyer tous les noms de ville commençant par le texte de recherche exact.
             //return _villes
-            //    .Where(v => v.StartsWith(mot))
-            //    .ToList();
+            //.Where(v => v.StartsWith(mot))
+            //.ToList();
 
             //Green 3 : La fonctionnalité de recherche doit être insensible à la casse
             //return _villes
@@ -43,9 +39,15 @@ namespace Exercices
             //.ToList();
 
             //Green 4 : La fonctionnalité de recherche devrait également fonctionner lorsque le texte de recherche n'est qu'une partie d'un nom de ville
-            return _villes
-            .Where(v => v.StartsWith(mot, StringComparison.OrdinalIgnoreCase)|| v.IndexOf(mot, StringComparison.OrdinalIgnoreCase) >= 0)
-            .ToList();
+            //return _villes
+            //.Where(v => v.StartsWith(mot, StringComparison.OrdinalIgnoreCase)|| v.IndexOf(mot, StringComparison.OrdinalIgnoreCase) >= 0)
+            //.ToList();
+
+            //Green 5 : Si le texte de recherche est un « * » (astérisque), il doit renvoyer tous les noms de ville.
+            //if (mot == "*")
+            //    return new List<string>(_villes);
+
+
 
         }
 
