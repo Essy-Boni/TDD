d1965d417cfdea77740fcf91240a2a0030e55774
Essy-Boni <Esaboni.pro@gmail.com>
2026-02-27 14:44:28 +0100

    Exercice4 - Bowling + Utilisation Moq

--- Exercice 4/Exercice 4/BowlingApp.Test/FrameTest.cs
+++ Exercice 4/Exercice 4/BowlingApp.Test/FrameTest.cs
@@ -1,75 +1,97 @@
+using Moq;
+
 namespace BowlingApp.Test;
 
 [TestClass]
 public class FrameTest
 {
 
-
-
     [TestMethod] // Série standard : Le premier lancer d’une série doit augmenter le score de la série
     public void Roll_SimpleFrame_FirstRoll_CheckScore()
     {
-        var gen = new GenerateurMock(4);
-        var frame = new Frame(gen, false);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen)
+            .Setup(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(4);
 
-        var result = frame.MakeRoll(); 
+        var frame = new Frame(fakeGen, lastFrame: false);
 
-        Assert.IsTrue(result); // 1er tour => result 4
+        Assert.IsTrue(frame.MakeRoll()); // 1er tour => result 4
         Assert.AreEqual(4, frame.Score); // Score = +4
     }
 
     [TestMethod] // Série standard : Le second lancer d’une série doit augmenter le score de cette série
     public void Roll_SimpleFrame_SecondRoll_CheckScore()
     {
-        var gen = new GenerateurMock(4, 3);
-        var frame = new Frame(gen, false);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen)
+            .SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(4) // 1er tour => result 4
+            .Returns(3); // 2eme tour => result 3
 
-        frame.MakeRoll(); // 1er tour => result 4
-        frame.MakeRoll(); // 2eme tour => result 3
+        var frame = new Frame(fakeGen, lastFrame: false);
 
-        Assert.AreEqual(7, frame.Score); // Score = 4 + 3 
+        frame.MakeRoll();
+        frame.MakeRoll();
+
+        Assert.AreEqual(7, frame.Score);
     }
 
     [TestMethod] // Série standard : En cas de strike, il ne doit pas être possible de lancer de nouveau au cours de cette même série
     public void Roll_SimpleFrame_SecondRoll_FirstRollStrick_ReturnFalse()
     {
-        var gen = new GenerateurMock(10);
-        var frame = new Frame(gen, false);
+       
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).Setup(g => g.RandomPin(It.IsAny<int>())).Returns(10);
+
+        var frame = new Frame(fakeGen, lastFrame: false);
 
         Assert.IsTrue(frame.MakeRoll()); //1er tour : result = 10 (strike) 
         Assert.IsFalse(frame.MakeRoll()); // Lancer impossible
+        Assert.AreEqual(10, frame.Score);
     }
 
     [TestMethod]
     public void Roll_SimpleFrame_MoreRolls_ReturnFalse() // Série standard : il ne doit pas être possible de lancer plus de 2 fois
     {
-        var gen = new GenerateurMock(3, 4);
-        var frame = new Frame(gen, false);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(3) // 1er tour => result 3 
+            .Returns(4); // 2eme tour => result 4 (score = 7, il reste 3 quilles)
 
-        Assert.IsTrue(frame.MakeRoll()); // 1er tour => result 3 
-        Assert.IsTrue(frame.MakeRoll()); // 2eme tour => result 4 (score = 7, il reste 3 quilles)
+        var frame = new Frame(fakeGen, lastFrame: false);
+
+        Assert.IsTrue(frame.MakeRoll());
+        Assert.IsTrue(frame.MakeRoll());
         Assert.IsFalse(frame.MakeRoll()); // Lancer impossible
-
     }
 
     [TestMethod] // Série Finale : En cas de strike, il doit être possible de lancer une nouvelle fois au cours d’une série
     public void Roll_LastFrame_SecondRoll_FirstRollStrick_ReturnTrue()
     {
-        var gen = new GenerateurMock(10, 3);
-        var frame = new Frame(gen, true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(10) // 1ère série = strike
+            .Returns(3);
 
-        Assert.IsTrue(frame.MakeRoll()); // 1ère série = strike
+        var frame = new Frame(fakeGen, lastFrame: true);
+
+        Assert.IsTrue(frame.MakeRoll());
         Assert.IsTrue(frame.MakeRoll()); // 2ème lancé possible
     }
 
     [TestMethod] // Série Finale : En cas de strike puis de lancer, le score est censé augmenter en accord avec le résultat du lancer
     public void Roll_LastFrame_SecondRoll_FirstRollStrick_CheckScore()
     {
-        var gen = new GenerateurMock(10, 3);
-        var frame = new Frame(gen, lastFrame: true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(10)  // 1ère serie = strike
+            .Returns(3); // 2ème serie, score = 3
 
-        frame.MakeRoll(); // 1ère serie = strike
-        frame.MakeRoll(); // 2ème serie, score = 3
+        var frame = new Frame(fakeGen, lastFrame: true);
+
+        frame.MakeRoll();
+        frame.MakeRoll();
 
         Assert.AreEqual(13, frame.Score); // Résultat final = 10 + 3 
     }
@@ -77,20 +99,29 @@ public class FrameTest
     [TestMethod] // Série finale : En cas de strike puis d’un lancer, il doit être possible de lancer une nouvelle fois
     public void Roll_LastFrame_ThirdRoll_FirstRollStrick_ReturnTrue()
     {
-        var gen = new GenerateurMock(10, 3, 5);
-        var frame = new Frame(gen, true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(10) // 1er lancé = strike 
+            .Returns(3) // 2eme lancé 
+            .Returns(5);
 
-        frame.MakeRoll(); // 1er lancé = strike 
-        frame.MakeRoll(); // 2eme lancé 
+        var frame = new Frame(fakeGen, lastFrame: true);
 
+        Assert.IsTrue(frame.MakeRoll());
+        Assert.IsTrue(frame.MakeRoll());
         Assert.IsTrue(frame.MakeRoll()); // 3ème lancé autorisé
     }
 
     [TestMethod] // Série finale : En cas de spare puis de lancer, le score est censé augmenter en accord avec le résultat du lancer
     public void Roll_LastFrame_ThirdRoll_Spare_CheckScore()
     {
-        var gen = new GenerateurMock(4, 6, 5); 
-        var frame = new Frame(gen, lastFrame: true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(4)
+            .Returns(6) 
+            .Returns(5); 
+        
+        var frame = new Frame(fakeGen, lastFrame: true);
 
         frame.MakeRoll(); // 4
         frame.MakeRoll(); // 6 => score = spare (10)
@@ -102,12 +133,17 @@ public class FrameTest
     [TestMethod] // Série finale : En cas de strike puis de lancer, le score est censé augmenter en accord avec le résultat
     public void Roll_LastFrame_ThirdRoll_FirstRollStrick_CheckScore()
     {
-        var gen = new GenerateurMock(10, 3, 5);
-        var frame = new Frame(gen, lastFrame: true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(10)
+            .Returns(3)
+            .Returns(5);
 
-        frame.MakeRoll(); // Strike
-        frame.MakeRoll(); // 3
-        frame.MakeRoll(); // 5
+        var frame = new Frame(fakeGen, lastFrame: true);
+
+        frame.MakeRoll();
+        frame.MakeRoll();
+        frame.MakeRoll();
 
         Assert.AreEqual(18, frame.Score); // resultat = 10 + 3 + 5
     }
@@ -115,19 +151,30 @@ public class FrameTest
     [TestMethod] // Série finale :  En cas de spare, il doit être possible de lancer une nouvelle fois au cours d’une série
     public void Roll_LastFrame_ThirdRoll_Spare_ReturnTrue()
     {
-        var gen = new GenerateurMock(4, 6, 5); 
-        var frame = new Frame(gen, lastFrame: true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen).SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(4) // 4
+            .Returns(6) // 6 => spare
+            .Returns(5);
 
-        Assert.IsTrue(frame.MakeRoll()); // 4
-        Assert.IsTrue(frame.MakeRoll()); // 6 => spare
+        var frame = new Frame(fakeGen, lastFrame: true);
+
+        Assert.IsTrue(frame.MakeRoll());
+        Assert.IsTrue(frame.MakeRoll());
         Assert.IsTrue(frame.MakeRoll()); // 3ème lancé autorisé
     }
 
     [TestMethod] // Série finale :  En cas de lancers standards, il ne doit pas être possible de lancer plus de 4 fois
     public void Roll_LastFrame_FourthRoll_ReturnFalse()
     {
-        var gen = new GenerateurMock(10, 3, 5);
-        var frame = new Frame(gen, true);
+        IGenerateur fakeGen = Mock.Of<IGenerateur>();
+        Mock.Get(fakeGen)
+            .SetupSequence(g => g.RandomPin(It.IsAny<int>()))
+            .Returns(10)
+            .Returns(3)
+            .Returns(5);
+
+        var frame = new Frame(fakeGen, lastFrame: true);
 
         frame.MakeRoll(); // 1er lancé
         frame.MakeRoll(); // 2e lancé
@@ -137,3 +184,5 @@ public class FrameTest
     }
 
 }
+
+
